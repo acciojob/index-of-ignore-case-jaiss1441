9ac35e22028b4b8d4dcc9b3bdac6f167cf760e07
Jaiswal Vikas <jaiswalvikas7276@gmail.com>
2024-11-13 06:38:12 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function indexOfIgnoreCase(s1, s2) {
+	  const lowerS1 = s1.toLowerCase();
+    const lowerS2 = s2.toLowerCase();
+
+    // Use indexOf to find the first occurrence of lowerS2 in lowerS1
+    return lowerS1.indexOf(lowerS2);
   // write your code here
 }
 
